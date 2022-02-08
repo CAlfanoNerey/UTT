d62fdd3a9f4c83867b06d488a452143e53597e5b
ismabug <ismabug@gmail.com>
2022-02-07 22:00:12 -0500

    Linked and setup Login/Logout

--- seniorProj/mysite/scheduler/urls.py
+++ seniorProj/mysite/scheduler/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 
 urlpatterns = [
     path('', views.indexView, name='home'),
-    path('signup/',views.signUpView, name='signup')
+    path('signup/',views.signUpView, name='signup'),
+    path('login/', views.loginView, name='login'),
+    path('logout/', LogoutView.as_view(), name="logout"),
 ]
