72951e2e7a15e19fd8cf4bfc5f4d2d055b05e4e7
CAlfanoNerey <Calfano1999@gmail.com>
2022-03-22 12:52:09 -0400

    fixed models to allow null values for days time duration location and comment

--- seniorProj/mysite/scheduler/models.py
+++ seniorProj/mysite/scheduler/models.py
@@ -9,6 +9,7 @@ from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 
 
+
 class fullClass (models.Model):
     crn = models.IntegerField()
     subj = models.CharField(max_length=50)
@@ -17,11 +18,12 @@ class fullClass (models.Model):
     title = models.CharField(max_length=200)
     max = models.IntegerField()
     seatsTaken = models.IntegerField(default=0)
-    days = models.CharField(max_length=7)
-    time = models.TimeField()
-    duration = models.TimeField()
+    days = models.CharField(max_length=7, blank=True, null=True)
+    time = models.TimeField(blank=True, null=True)
+    duration = models.TimeField(blank=True, null=True)
+    location = models.CharField(max_length=50, blank=True)
     instructor = models.CharField(max_length=50)
-    addInfo = models.TextField(max_length=500)
+    addInfo = models.TextField(max_length=500, blank=True)
     def __str__(self):
         return str(self.crn)
 
